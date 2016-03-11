7148ce415aa4d073a9bcecd8cd4e544fb9b41f2a
cassandrabarbe <cassandra.barbe@gmail.com>
2016-03-11 14:17:30 -0500

    i don't know

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-              var $ul = $('ul');
+    var $ul = $('ul');
 
-              grains.forEach (function(grain){
-              var $name = $('<h2>');
-              var $img = $('<img>');
-              var $desc = $('<p>');
-              var $li = $('<li>');
+    grains.forEach (function(grain){
+    var $name = $('<h2>');
+    var $img = $('<img>');
+    var $desc = $('<p>');
+    var $li = $('<li>');
 
-              $img.attr('src', 'images/' + grain.img);
-              $name.html(grain.name);
-              $desc.html(grain.desc);
+    $img.attr('src', 'images/' + grain.img);
+    $name.html(grain.name);
+    $desc.html(grain.desc);
 
-              $li.append($img);
-              $li.append($name);
-              $li.append($desc);
-              $ul.append($li);
-              });
+    $li.append($img);
+    $li.append($name);
+    $li.append($desc);
+    $ul.append($li);
+    });
