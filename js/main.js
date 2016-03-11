01265583b018211df292cf245812a8bc82b0ce30
cassandrabarbe <cassandra.barbe@gmail.com>
2016-03-11 10:51:28 -0500

    i don't know

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var $ul = $('ul');
 
-.grains.forEach(function (grain){
+grains.forEach(function (grain){
   var $name = $('<h2>');
   var $img = $('<img>');
   var $desc = $('<p>');
   var $li = $('<li>');
 
-  $img.attr('src' 'images/' +grain.img);
+  $img.attr('src', 'images/' +grain.img);
   $name.html(grain.name);
   $desc.html(grain.desc);
 
